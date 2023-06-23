0285ef917ecd50578bbf89c0486f006dc7ed58ac
LINUX <linux@192.168.1.20>
2023-06-23 16:09:51 +0300

    First Commit

---
+++ app/Modules/Dashboard/Controllers/Dashboard.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modules\Dashboard\Controllers;
+
+use CodeIgniter\Controller;
+
+class Dashboard extends Controller
+{
+    public function index()
+    {
+        // Logic for fetching data and rendering the view
+        return view('Modules/Dashboard/Views/dashboard');
+    }
+}
---
+++ app/Modules/Dashboard/Views/Dashboard.php
@@ -0,0 +1,2 @@
+<h1>Welcome to the Dashboard</h1>
+<!-- Add your Dashboard view content here -->
